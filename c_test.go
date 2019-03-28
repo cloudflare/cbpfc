05dbc38ce4fb92210019f45c75021b5c2ce32c60
Arthur Fabre <afabre@cloudflare.com>
2019-03-28 21:56:53 +0000

    Add basic examples
    Add basic examples that show how to use cbpfc based on the test suite
    that builds programs to test ToC() and ToEBPF().
    
    Add a separate helper in each example that is used by the example
    function, as well as the tests. This reduces code duplication, and
    ensures the examples work properly - the generated programs are tested.
    
    It's not clear what we could usefully print in the examples to use with:
    
        // Output:
    
    So the example functions are only compiled, not run. They're both only
    call the helper functions though, so I think this is ok.

--- c_test.go
+++ c_test.go
@@ -2,11 +2,7 @@ package cbpfc
 
 import (
 	"bytes"
-	"os"
 	"testing"
-	"text/template"
-
-	"github.com/cloudflare/cbpfc/clang"
 
 	"github.com/newtools/ebpf"
 	"golang.org/x/net/bpf"
@@ -34,93 +30,14 @@ func TestFunctionName(t *testing.T) {
 	checkName(t, "a2", true)
 }
 
-// Env var of clang binary to use
-const clangEnv = "CLANG"
-
-const (
-	filterName = "filter"
-	entryPoint = "xdp_filter"
-)
-
-var testTemplate = template.Must(template.New(entryPoint).Parse(`
-#define __section(NAME) __attribute__((section(NAME), used))
-
-char __license[] __section("license") = "BSD";
-
-typedef unsigned int uint32_t;
-typedef unsigned short uint16_t;
-typedef unsigned char uint8_t;
-
-typedef char bool;
-#define false 0
-#define true 1
-
-#define ntohs __builtin_bswap16
-#define ntohl __builtin_bswap32
-
-struct xdp_md {
-	uint32_t data;
-	uint32_t data_end;
-};
-
-enum xdp_action {
-	XDP_DROP = 1,
-	XDP_PASS,
-};
-
-{{.Filter}}
-
-__section("xdp") int xdp_filter(struct xdp_md *ctx) {
-	uint8_t *data = (uint8_t *)(long)ctx->data;
-	uint8_t const *data_end = (uint8_t *)(long)ctx->data_end;
-
-	if ({{.FilterName}}(data, data_end)) {
-		return XDP_DROP;
-	}
-
-	return XDP_PASS;
-}
-`))
-
-type testTemplateOpts struct {
-	// Definition of the filter
-	Filter string
-
-	// Name of the filter
-	FilterName string
-}
+const entryPoint = "xdp_filter"
 
 // loadC compiles classic BPF to C, which is compiled with clang
 // XDPDrop on match, XDPPass otherwise
 func loadC(tb testing.TB, insns []bpf.Instruction) *ebpf.ProgramSpec {
 	tb.Helper()
 
-	// generate C
-	filter, err := ToC(insns, COpts{
-		FunctionName: filterName,
-	})
-	if err != nil {
-		tb.Fatal(err)
-	}
-
-	c := bytes.Buffer{}
-	err = testTemplate.Execute(&c, testTemplateOpts{
-		Filter:     filter,
-		FilterName: filterName,
-	})
-	if err != nil {
-		tb.Fatal(err)
-	}
-
-	// compile to ELF
-	clangBin, ok := os.LookupEnv(clangEnv)
-	if !ok {
-		clangBin = "/usr/bin/clang"
-	}
-
-	elf, err := clang.Compile(c.Bytes(), entryPoint, clang.Opts{
-		Clang: clangBin,
-	})
+	elf, err := buildC(insns, entryPoint)
 	if err != nil {
 		tb.Fatal(err)
 	}
--- ebpf_test.go
+++ ebpf_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/newtools/ebpf"
-	"github.com/newtools/ebpf/asm"
 	"golang.org/x/net/bpf"
 )
 
@@ -13,57 +12,9 @@ import (
 func loadEBPF(tb testing.TB, insns []bpf.Instruction) *ebpf.ProgramSpec {
 	tb.Helper()
 
-	// Labels of blocks
-	exit := "exit"
-	match := "match"
-
-	filter, err := ToEBPF(insns, EBPFOpts{
-		PacketStart:  asm.R0,
-		PacketEnd:    asm.R1,
-		RegA:         asm.R2,
-		RegX:         asm.R3,
-		RegTmp:       asm.R4,
-		RegIndirect:  asm.R5,
-		LabelPrefix:  "filter",
-		MatchLabel:   match,
-		NoMatchLabel: exit,
-	})
+	prog, err := buildEBPF(insns)
 	if err != nil {
 		tb.Fatal(err)
-	}
-
-	prog := asm.Instructions{
-		// Save ctx
-		asm.Mov.Reg(asm.R6, asm.R1),
-
-		// Packet start
-		asm.LoadMem(asm.R0, asm.R6, 0, asm.Word),
-
-		// Packet end
-		asm.LoadMem(asm.R1, asm.R6, 4, asm.Word),
-
-		// Fall through to filter
-	}
-
-	prog = append(prog, filter...)
-
-	// verifier does not like dead code - only include exit blocks if the prog refers to them
-	refs := prog.ReferenceOffsets()
-
-	// Match
-	if _, ok := refs[match]; ok {
-		prog = append(prog,
-			asm.Mov.Imm(asm.R0, int32(XDPDrop)).Sym(match),
-			asm.Return(),
-		)
-	}
-
-	// Exit
-	if _, ok := refs[exit]; ok {
-		prog = append(prog,
-			asm.Mov.Imm(asm.R0, int32(XDPPass)).Sym(exit),
-			asm.Return(),
-		)
 	}
 
 	tb.Logf("\n%v", prog)
