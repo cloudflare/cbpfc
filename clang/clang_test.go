7ac485fd93e1ed741c107038b46abf2f443e8e5f
Arthur Fabre <afabre@cloudflare.com>
2026-08-04 17:51:42 +0200

    clang: Use stdin / stdout for clang without Output directory
    If the opts.Output directory isn't set, use stdin to pass the source to
    clang, and read the ELF from stdout.
    
    This avoids needing to write to temporary files.

--- clang/clang_test.go
+++ clang/clang_test.go
@@ -1,7 +1,10 @@
 package clang
 
 import (
+	"bytes"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -40,4 +43,34 @@ func TestCompile(t *testing.T) {
 			t.Fatal("CPUTime zero")
 		}
 	})
+
+	t.Run("output_dir", func(t *testing.T) {
+		opts := opts
+
+		output := t.TempDir()
+		opts.Output = output
+
+		elf, err := Compile(source, "test", opts)
+		if err != nil {
+			t.Fatalf("error: %v", err)
+		}
+
+		if !bytes.Equal(source, mustReadFile(t, output, "test.c")) {
+			t.Fatal("source doesn't match")
+		}
+		if !bytes.Equal(elf, mustReadFile(t, output, "test.elf")) {
+			t.Fatal("Returned ELF and filesystem ELF are different")
+		}
+		if build := string(mustReadFile(t, output, "build")); !strings.Contains(build, "clang") {
+			t.Fatalf("bad build script: %q", build)
+		}
+	})
+}
+
+func mustReadFile(tb testing.TB, dir string, file string) []byte {
+	val, err := os.ReadFile(filepath.Join(dir, file))
+	if err != nil {
+		tb.Fatal(err)
+	}
+	return val
 }
